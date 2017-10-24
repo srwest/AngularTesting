5d1736650abc68c4b6f4b0b4510683d39bc7376a
srwest <31323196+srwest@users.noreply.github.com>
2017-10-24 15:32:02 -0400

    Add files via upload

---
+++ src/app/app.module.ts
@@ -0,0 +1,33 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+
+import { AppComponent } from './app.component';
+import { SearchComponent } from './search/search.component';
+import { EditComponent } from './edit/edit.component';
+import { Routes, RouterModule } from '@angular/router';
+import { SearchService } from './shared';
+
+const appRoutes: Routes = [
+  { path: 'search', component: SearchComponent },
+  { path: 'edit/:id', component: EditComponent },
+  { path: '', redirectTo: '/search', pathMatch: 'full' }
+];
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    SearchComponent,
+    EditComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    RouterModule.forRoot(appRoutes)
+  ],
+  providers: [SearchService],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
